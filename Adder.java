9f4e2109057dec60320c4a72847336307287b67c
Watthana Somphong <60050251@kmitl.ac.th>
2018-09-10 18:47:19 +0700

    add : adder.java

---
+++ Adder.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Adder{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x,y,z;
+        x = sc.nextInt();
+        y = sc.nextInt();
+        z = x+y;
+    System.out.println(z);
+}
+
+}
